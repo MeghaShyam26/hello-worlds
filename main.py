0fba5c9f35a1204e7e013742e987dd328525ec7a
MeghaShyam26 <meghashyam138@gmail.com>
2020-09-18 01:59:50 +0530

    add updated main.py to github

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = os.environ.get('NAME', 'World')
-    return 'This is shyam {}!'.format(name)
+    return 'This shyam {}!'.format(name)
 
 
 if __name__ == "__main__":
